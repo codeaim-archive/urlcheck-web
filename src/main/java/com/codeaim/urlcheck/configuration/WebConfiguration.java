ad8ca3415a44f05950cca96b642bc6748f33bfd3
Gerard Downes <gdownes@gmail.com>
2016-11-18 16:43:02 +0000

    Reworking login screen

--- src/main/java/com/codeaim/urlcheck/configuration/WebConfiguration.java
+++ src/main/java/com/codeaim/urlcheck/configuration/WebConfiguration.java
@@ -11,9 +11,9 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "urlcheck.web")
 public class WebConfiguration
 {
-    private String getChecksByUsernameEndpoint = "http://localhost:6601/check/{username}";
-    private String createCheckEndpoint = "http://localhost:6601/check/{username}";
-    private String deleteCheckEndpoint = "http://localhost:6601/check/{username}/{id}";
-    private String updateCheckEndpoint = "http://localhost:6601/check/{username}/{id}";
-    private String getUserByUsernameEndpoint = "http://localhost:6601/user/{username}";
+    private String getChecksByUsernameEndpoint = "http://api.urlcheck.io/check/{username}";
+    private String createCheckEndpoint = "http://api.urlcheck.io/check/{username}";
+    private String deleteCheckEndpoint = "http://api.urlcheck.io/check/{username}/{id}";
+    private String updateCheckEndpoint = "http://api.urlcheck.io/check/{username}/{id}";
+    private String getUserByUsernameEndpoint = "http://api.urlcheck.io/user/{username}";
 }
