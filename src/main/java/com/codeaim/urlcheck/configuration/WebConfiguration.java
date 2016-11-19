be3ff41df0ecb5b8afe17191d45905704327a1d2
GDownes <gdownes@gmail.com>
2016-11-19 00:06:18 +0000

    Adding email verification endpoint

--- src/main/java/com/codeaim/urlcheck/configuration/WebConfiguration.java
+++ src/main/java/com/codeaim/urlcheck/configuration/WebConfiguration.java
@@ -11,15 +11,9 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "urlcheck.web")
 public class WebConfiguration
 {
-    private String getChecksByUsernameEndpoint = "http://localhost:6601/check/{username}";
-    private String createCheckEndpoint = "http://localhost:6601/check/{username}";
-    private String deleteCheckEndpoint = "http://localhost:6601/check/{username}/{id}";
-    private String updateCheckEndpoint = "http://localhost:6601/check/{username}/{id}";
-    private String getUserByUsernameEndpoint = "http://localhost:6601/user/{username}";
-    private String verifyUserEmailEndpoint = "http://localhost:6601/user/{username}/verify";
     private String getChecksByUsernameEndpoint = "http://api.urlcheck.io/check/{username}";
     private String createCheckEndpoint = "http://api.urlcheck.io/check/{username}";
     private String deleteCheckEndpoint = "http://api.urlcheck.io/check/{username}/{id}";
     private String updateCheckEndpoint = "http://api.urlcheck.io/check/{username}/{id}";
     private String getUserByUsernameEndpoint = "http://api.urlcheck.io/user/{username}";
-}
+    private String verifyUserEmailEndpoint = "http://api.urlcheck.io/user/{username}/verify";}
