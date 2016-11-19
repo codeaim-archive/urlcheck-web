95beb836d3347e1cd5def7e771b05af733f4456e
GDownes <gdownes@gmail.com>
2016-11-19 22:01:56 +0000

    Adding email verification resend

--- src/main/java/com/codeaim/urlcheck/model/Verification.java
+++ src/main/java/com/codeaim/urlcheck/model/Verification.java
@@ -1,5 +1,12 @@
 package com.codeaim.urlcheck.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Accessors(chain = true)
 public class Verification
 {
 }
